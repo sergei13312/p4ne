9aac9e3583fb8b09f98e445bf40d5b070d4d9de5
Sergey <sergei31131@gmail.com>
2021-04-20 19:14:51 +0300

    test1

--- Lab1.2/Lab1.2.py
+++ Lab1.2/Lab1.2.py
@@ -2,12 +2,10 @@
 from matplotlib import pyplot
 from openpyxl import load_workbook
 
-def getvalue(x):
-    return x.value
 wb = load_workbook("data_analysis_lab.xlsx")
 sheet = wb["Data"]
-
-
+def getvalue(x):
+    return x.value
 Years = list(map(getvalue,sheet['A'][1:]))
 Temperature = list(map(getvalue,sheet['C'][1:]))
 Activity = list(map(getvalue, sheet['D'][1:]))
@@ -15,4 +13,9 @@ Activity = list(map(getvalue, sheet['D'][1:]))
 
 pyplot.plot(Years,Temperature,label="Относит. Температура")
 pyplot.plot(Years,Activity,label="Активность Солнца")
+
+pyplot.legend(loc='upper left')
+
+
+
 pyplot.show()
